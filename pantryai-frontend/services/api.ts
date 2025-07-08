d86350267c6c79234555124a66054114d6c7a615
Chetan Goel <chetan.goel2019@gmail.com>
2025-07-08 14:15:47 -0400

    update api route

--- pantryai-frontend/services/api.ts
+++ pantryai-frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { getDeviceId } from './getDeviceId';
 
-const API_BASE_URL = 'https://pantryai.onrender.com';
+const API_BASE_URL = 'https://pantryai.dragonchetan.com';
 
 const api = axios.create({
     baseURL: API_BASE_URL,
